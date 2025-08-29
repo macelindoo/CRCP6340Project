c21123304f059ba8899e135c8b43632684ab9114
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-08-28 23:05:53 -0500

    added css, favicon, index.js, contact, project, projects, newProject

---
+++ js/index.js
@@ -0,0 +1,7 @@
+(function () {
+    document.querySelector("#testButton").addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("Seriously, thank you for clicking.");
+}
+})();
