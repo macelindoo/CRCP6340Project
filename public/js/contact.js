65aaa840eaed151ad1982b4243c353cc6994bab3
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-04 17:43:23 -0500

    Updated contact form w/Bootstrap's contact form, updated contact.js to work w/that. Updated CSS to override bootstrap default colors for form.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,19 +1,37 @@
 (function () {
 "use strict";
 
+    let form = document.querySelector('#contact-form');
+
     document
     .querySelector("#contact-form-button")
     .addEventListener("click", (event) => {
     event.preventDefault();
     event.stopPropagation();
+    let formValid = true;
+    if (!form.checkValidity()) {
+      formValid = false;
+    }
+    form.classList.add('was-validated')
+    if (formValid) {
+        sendTheEmail();
+    }
+});
+
+function sendTheEmail() {
     console.log("You clicked the submit button.");
-    let name = document.querySelector("#name").value;
+    let firstName = document.querySelector("#first-name").value;
+    let lastName = document.querySelector("#last-name").value;
     let email = document.querySelector("#mail").value;
     let message = document.querySelector("#msg").value;
-    console.log("Name: " + name);
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
     console.log("Email: " + email);
     console.log("Message: " + message);
-});
+}
+
+})()
+
     //This below is the same as what's written above, but above uses an arrow function rather than a named function
     // document
     // .querySelector("#contact-form-button")
@@ -31,4 +49,4 @@
 //     console.log("Message: " + message);
 // }
 
-})();
+// })();
