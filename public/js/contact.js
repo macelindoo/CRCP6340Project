335d6db11e7f5ae586b79689627e1ec06c19a97f
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-05 12:30:40 -0500

    added fetch to contact.js, updated html pages with Bootstrap Bundle & Custom Page Script. Moved newProject.html into public folder.

--- public/js/contact.js
+++ public/js/contact.js
@@ -20,16 +20,40 @@
 
 function sendTheEmail() {
     console.log("You clicked the submit button.");
-    let firstName = document.querySelector("#first-name").value;
-    let lastName = document.querySelector("#last-name").value;
-    let email = document.querySelector("#mail").value;
-    let message = document.querySelector("#msg").value;
-    console.log("First Name: " + firstName);
-    console.log("Last Name: " + lastName);
-    console.log("Email: " + email);
-    console.log("Message: " + message);
-}
+    // let firstName = document.querySelector("#first-name").value;
+    // let lastName = document.querySelector("#last-name").value;
+    // let email = document.querySelector("#mail").value;
+    // let message = document.querySelector("#msg").value;
+    let obj = {
+        sub: "Someone submitted a contact form from NFT site!",
+        txt: `${document.querySelector("#first-name").value} 
+        ${document.querySelector("#last-name").value} sent you a message that reads... ${document.querySelector("#msg").value} 
+        They're email address is ${document.querySelector("#mail").value}`,
+    };
+    // console.log(obj);
+    // console.log("First Name: " + firstName);
+    // console.log("Last Name: " + lastName);
+    // console.log("Email: " + email);
+    // console.log("Message: " + message);
 
+    fetch("/mail", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+    })
+      .then((r) => r.json())
+      .then((response) => {
+          document.querySelector("#contact-button-response").innerHTML = 
+          response.result;
+      })
+      .then(() => {
+          setTimeout(() => {
+              document.querySelector("#contact-button-response").innerHTML = "";
+          }, "5000");
+      });
+    }
 })()
 
     //This below is the same as what's written above, but above uses an arrow function rather than a named function
