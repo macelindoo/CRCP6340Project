09ab0a6dc0fd81abcb3af6cc19ed6dac9c4f76d9
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-04 20:25:28 -0500

    wrote mail send functionality into app & utils

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
 dotenv.config();
 
 const app = express();
@@ -11,11 +12,17 @@ app.use(express.static("public"));
 //   res.send('Hello World!');
 // });
 
-app.post('/mail', (req, res) => {
-  console.log("mail button clicked");
+app.post('/mail', async (req, res) => {
+  await utils
+  .sendMessage(req.body.sub, req.body.txt)
+  .then(() => {
+    res.send({ result: "success" });
+  })
+  .catch(() => {
+    res.send({ result: "failure" });
+  });
 });
 
 app.listen(port, () => {
-  console.log(process.env.SENSITIVE_INFO);
   console.log(`Example app listening on port ${port}`)
 });
