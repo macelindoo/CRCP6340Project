6d18149536559418504e57e408e9f95756e4ecb0
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-04 19:22:58 -0500

    installed nodemailer

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use(express.static("public"));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
 
-// app.post('/mail', (req, res) => {
-//   console.log("mail button clicked");
-// });
+app.post('/mail', (req, res) => {
+  console.log("mail button clicked");
+});
 
 app.listen(port, () => {
   console.log(process.env.SENSITIVE_INFO);
