53883f76f2051da6228de7ad6606b3994548132a
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-09 20:19:39 -0500

    Updated utils.js to get my email client working with contact form.

--- utils/utils.js
+++ utils/utils.js
@@ -1,16 +1,16 @@
 import nodemailer from "nodemailer";
 
 export async function sendMessage(sub, txt) {
-const nodemailer = require("nodemailer");
+//const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE,
+  port: Number(process.env.MAIL_PORT),
+  secure: process.env.MAIL_SECURE === 'true',
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
   },
-  requireTLS: process.env.MAIL_TLS, 
+  requireTLS: process.env.MAIL_TLS === 'true',
     });
 
     let message = {
@@ -19,13 +19,12 @@ const transporter = nodemailer.createTransport({
         subject: sub,
         text: txt,
     };
-    
-    await transporter
-    .sendMail(message)
-    .then(() => {
-        console.log("Message sent");
-    })
-    .catch((err) => {
-        console.error("Message not sent - " + err);
-    });
+
+   try {
+    await transporter.sendMail(message);
+    console.log("Message sent");
+  } catch (err) {
+    console.error("Message not sent - " + err);
+    throw err;
+  }
 }
