d7dcfeb2d069f7aa6407b23bc1c4fe71bc76ea7a
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-16 20:35:36 -0500

    Added comments for clarity to my app.js file. Routed all of my project pages in app.js using ejs. Updated all the relative paths to absolute paths for page content. Added individual project pages and an error message path for my Projects page.

--- app.js
+++ app.js
@@ -13,19 +13,41 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
   res.render("index.ejs");
 });
+
+/*-- Route for All Projects page --*/
 app.get("/projects", (req, res) => {
   res.render("projects.ejs", { projectArray: data });
 });
-app.get("/project", (req, res) => {
-  res.render("project.ejs");
+
+/*-- Route for individual project pages --*/
+app.get("/project/:id", (req, res) => {
+    let id = req.params.id;
+    if(id > data.length) {
+        throw new Error("No project with that ID");
+    }
+    res.render("project.ejs", {projectArray: data, which: id});
 });
+/*-- Route for Featured Project page --*/
+app.get("/project", (req, res) => {
+  res.render("project.ejs", { which: "Featured Project" });
+});
+
+/*-- Route for All Projects page --*/
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs");
+});
+
+/*-- Route for Contact page --*/
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
+
+/*-- Route for Project Creation page --*/
 app.get("/newProject", (req, res) => {
   res.render("newProject.ejs");
 });
 
+/*-- Response for contact form submission --*/
 app.post('/mail', async (req, res) => {
   await utils
   .sendMessage(req.body.sub, req.body.txt)
@@ -36,6 +58,13 @@ app.post('/mail', async (req, res) => {
     res.send({ result: "Message failure." });
   });
 });
+
+/*-- Error handling project pages outside the array range for individual projects --*/
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.render("error.ejs");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
