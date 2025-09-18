c87385531de1c53c01954925e2b8dbdefb9df531
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-09-17 19:46:07 -0500

    Fixed nav menu to highlight current page again with new ejs routes.

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render("index.ejs");
+// app.get('/', (req, res) => {
+//   res.render("index.ejs");
+// });
+
+/*-- Route for Home page --*/
+app.get("/", (req, res) => {
+  res.render("index.ejs", { activePage: "home"});
 });
 
 /*-- Route for All Projects page --*/
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs", { projectArray: data });
+  res.render("projects.ejs", { projectArray: data, activePage: "projects" });
 });
 
 /*-- Route for individual project pages --*/
@@ -25,26 +30,26 @@ app.get("/project/:id", (req, res) => {
     if(id > data.length) {
         throw new Error("No project with that ID");
     }
-    res.render("project.ejs", {projectArray: data, which: id});
+    res.render("project.ejs", {projectArray: data, which: id, activePage: "featured"});
 });
 /*-- Route for Featured Project page --*/
 app.get("/project", (req, res) => {
-  res.render("project.ejs", { which: "Featured Project" });
+  res.render("project.ejs", { which: "Featured Project", activePage: "featured" });
 });
 
 /*-- Route for All Projects page --*/
-app.get("/projects", (req, res) => {
-  res.render("projects.ejs");
-});
+// app.get("/projects", (req, res) => {
+//   res.render("projects.ejs");
+// });
 
 /*-- Route for Contact page --*/
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  res.render("contact.ejs", { activePage: "contact" });
 });
 
 /*-- Route for Project Creation page --*/
 app.get("/newProject", (req, res) => {
-  res.render("newProject.ejs");
+  res.render("newProject.ejs", { activePage: "newProject" });
 });
 
 /*-- Response for contact form submission --*/
