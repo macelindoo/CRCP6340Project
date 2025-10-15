c98d077722f1176f205eae03ec7d89ec1470e9bf
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-10-15 16:25:03 -0500

    final push prior to deployment

--- utils/database.js
+++ utils/database.js
@@ -17,19 +17,21 @@ export async function connect() {
     "/" +
     process.env.MYSQL_DATABASE;
     pool = mysql.createPool(
-        //cString); //digital ocean sql server
-        {
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DATABASE,
-            //port: process.env.MYSQL_PORT
-        }
+        cString //digital ocean sql server
+        // {
+        //     //obj - localhost sql server
+        //     host: process.env.MYSQL_HOST,
+        //     user: process.env.MYSQL_USER,
+        //     password: process.env.MYSQL_PASSWORD,
+        //     database: process.env.MYSQL_DATABASE,
+        //     //port: process.env.MYSQL_PORT
+        // }
     )
     .promise();
 }
 
 export async function getAllProjects() {
     const [rows] = await pool.query('SELECT * FROM projects;');
+        console.log('Projects fetched:', rows);//log the fetched projects to verify
     return rows;
 }
