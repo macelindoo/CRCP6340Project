5acee1815867747e17f3c69cb5f9583812ced9d4
Ilea Shutler <iloo@Ileas-MacBook-Pro.local>
2025-11-28 15:08:20 -0600

    Refactor routes in app.js and index.ejs to pass project, contract, and mint data from server to client for NFT mint tracking; update index.js for improved wallet and contract interaction

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ import * as db from "./utils/database.js";
 //let projects = [];//global array to hold project data
 import cors from "cors";
 
+let projects = [];
+let contracts = [];
+let mints = [];
+
+
 const app = express();//create an express application
 app.use(cors());
 const port = 3000;//port number for local server
@@ -18,19 +23,49 @@ app.use(express.static("public"));//serve static files (images, CSS, JS) from th
 //   res.render("index.ejs");
 // });
 
-/*-- Route for Home page --*/
+/*-- NEW Route for Home page --*/
 app.get("/", async (req, res, next) => {
   try {
-    await db.connect();//connect to the database (MYSQL server)
-    const projects = await db.getAllProjects();//fetch all projects as an array from the database
-    const featuredID = utils.getDailyFeaturedProjectId(projects);//get the daily featured project ID using a utility function
-    const featuredProject = projects.find(p => p.id === featuredID);//find the project with the matching ID
-    console.log(projects);//log the projects array to the console to verify data retrieval
-    res.render("index.ejs", { activePage: "home", featuredProject });//render the index.ejs template, passing the active page and the randomly selected featured project
-  } catch (err) {//catch any errors that occur during the process
-    next(err);//pass the error to the next middleware (error handler)
+    await db.connect();
+    const projects = await db.getAllProjects();
+
+    // Populate contracts and mints arrays
+    let contracts = [];
+    let mints = [];
+    projects.forEach((item) => {
+      contracts.push(item.contractAddress || ""); // Use "" if not present
+      mints.push(0); // Placeholder, will be updated client-side
+    });
+
+    // Pick featured project (random or daily)
+    const featuredID = utils.getDailyFeaturedProjectId(projects);
+    const featuredProject = projects.find(p => p.id === featuredID);
+
+    res.render("index.ejs", {
+      activePage: "home",
+      featuredProject,
+      contracts,
+      mints,
+      projects,
+    });
+  } catch (err) {
+    next(err);
   }
 });
+
+/*-- CURRENT Route for Home page --*/
+// app.get("/", async (req, res, next) => {
+//   try {
+//     await db.connect();//connect to the database (MYSQL server)
+//     const projects = await db.getAllProjects();//fetch all projects as an array from the database
+//     const featuredID = utils.getDailyFeaturedProjectId(projects);//get the daily featured project ID using a utility function
+//     const featuredProject = projects.find(p => p.id === featuredID);//find the project with the matching ID
+//     console.log(projects);//log the projects array to the console to verify data retrieval
+//     res.render("index.ejs", { activePage: "home", featuredProject });//render the index.ejs template, passing the active page and the randomly selected featured project
+//   } catch (err) {//catch any errors that occur during the process
+//     next(err);//pass the error to the next middleware (error handler)
+//   }
+// });
 
 // /*-- Route for Home page --*/
 // app.get("/", async (req, res, next) => {
@@ -45,16 +80,42 @@ app.get("/", async (req, res, next) => {
 //   }
 // });
 
-/*-- Route for All Projects page --*/
+/*-- NEW Route for All Projects page --*/
 app.get("/projects", async (req, res, next) => {
-    try {
+  try {
     await db.connect();
     const projects = await db.getAllProjects();
-    res.render("projects.ejs", { projectArray: projects, activePage: "projects" });
+
+    // Populate contracts and mints arrays from projects
+    let contracts = [];
+    let mints = [];
+    projects.forEach((item) => {
+      contracts.push(item.contractAddress || "");
+      mints.push(0); // Placeholder, will be updated client-side
+    });
+
+    res.render("projects.ejs", {
+      projectArray: projects,
+      activePage: "projects",
+      contracts,
+      mints,
+      projects,
+    });
   } catch (err) {
     next(err);
   }
 });
+
+// /*-- CURRENT Route for All Projects page --*/
+// app.get("/projects", async (req, res, next) => {
+//     try {
+//     await db.connect();
+//     const projects = await db.getAllProjects();
+//     res.render("projects.ejs", { projectArray: projects, activePage: "projects" });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 /*-- Route for Featured Project page --*/
 /*-- This must come before the route to individual project pages or nav menu link won't work because of route matching order --*/
@@ -71,23 +132,56 @@ app.get("/project/featured", async (req, res, next) => {
   }
 });
 
-/*-- Route for individual project pages --*/
-app.get("/project/:id", async(req, res, next) => {
-    try {
-    await db.connect();//connect to the database
-    const projects = await db.getAllProjects();//fetch all projects as an array from the database
-    let id = Number(req.params.id);//using Number to convert string to number//Get the project ID from the URL and convert it from a string to a number
+/*-- NEW Route for individual project pages --*/
+app.get("/project/:id", async (req, res, next) => {
+  try {
+    await db.connect();
+    const projects = await db.getAllProjects();
+
+    // Populate contracts and mints arrays from projects
+    let contracts = [];
+    let mints = [];
+    projects.forEach((item) => {
+      contracts.push(item.contractAddress || "");
+      mints.push(0); // Placeholder, will be updated client-side
+    });
+
+    let id = Number(req.params.id);
     let project = projects.find(p => p.id === id);
-      if (!project) {
-        res.status(404).render("404.ejs");
-        return;
-      }
-    console.log(project);//log the selected project to the console to verify correct retrieval
-    res.render("project.ejs", {project, which: id, activePage: "project"});//render the project.ejs template, passing the selected project, its ID, and the active page
-    } catch (err) {//catch any errors that occur during the process
-    next(err);//pass the error to the next middleware (error handler)
+    if (!project) {
+      res.status(404).render("404.ejs");
+      return;
+    }
+    res.render("project.ejs", {
+      project,
+      which: id,
+      activePage: "project",
+      contracts,
+      mints,
+      projects,
+    });
+  } catch (err) {
+    next(err);
   }
 });
+
+/*-- CURRENT Route for individual project pages --*/
+// app.get("/project/:id", async(req, res, next) => {
+//     try {
+//     await db.connect();//connect to the database
+//     const projects = await db.getAllProjects();//fetch all projects as an array from the database
+//     let id = Number(req.params.id);//using Number to convert string to number//Get the project ID from the URL and convert it from a string to a number
+//     let project = projects.find(p => p.id === id);
+//       if (!project) {
+//         res.status(404).render("404.ejs");
+//         return;
+//       }
+//     console.log(project);//log the selected project to the console to verify correct retrieval
+//     res.render("project.ejs", {project, which: id, activePage: "project"});//render the project.ejs template, passing the selected project, its ID, and the active page
+//     } catch (err) {//catch any errors that occur during the process
+//     next(err);//pass the error to the next middleware (error handler)
+//   }
+// });
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,62 @@
+import { ethers } from "./ethers-5.2.esm.js";
+import { contractABI } from "./contractABI.js";
+import "./ejs.js";
+
 "use strict";
 
-(() => {
-  let userAddress = null;
+export let provider = null;
+export let signer = null;
+export let userAddress = null;
+
+//Get project, contract, and mint data from div tags in index.ejs
+export let contractList = document
+  .querySelector("#contracts")
+  .innerHTML.split(",");
+export let mintList = document.querySelector("#mints").innerHTML.split(",");
+export let projectList = JSON.parse(
+  document.querySelector("#projects").innerHTML
+);
+
   let connect = document.querySelector("#wallet-connect");
-
-  if(window.ethereum) {
+  await connectWallet();
+  document.querySelector("#mints").innerHTML = mintList;
+  connect.addEventListener("click", async () => {
     connectWallet();
+  });
+  await updateMints();
+
+async function connectWallet() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    connect.innerHTML = "Connected";
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    userAddress = "" + accounts[0];
+    console.log("userAddress: ", userAddress);
+    let walletString =
+      userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+    connect.innerHTML = walletString;
+    console.log("provider: ", provider);
+    console.log("wallet: ", userAddress);
+    console.log("signer: ", signer);
   } else {
-    userAddress = null;
-    connect.innerHTML = "Connect Wallet";
+    connect.innerHTML = "Please install MetaMask";
   }
-
-    connect.addEventListener("click", async () => {
-      connectWallet();
-    });
-
-  async function connectWallet() {
-    await window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((data) => {
-            userAddress = data[0];
-            let walletString =
-                userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
-            connect.innerHTML = walletString;
-            return userAddress;
-         })
-        .catch((err) => {
-            if (err.code === 4001) {
-                // EIP-1193 userRejectedRequest error
-                // If this happens, the user rejected the connection request.
-                 console.log("Please connect a wallet");
-             } else {
-                console.error(err);
-            }
-        });
 }
-})();
+
+export async function updateMints() {
+  let newList = [];
+  for (let i = 0; i < contractList.length; i++) {
+    let contractAddress = contractList[i];
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    contract.connect(signer);
+    let num = await contract.totalSupply();
+    newList.push(num.toString());
+  }
+  mintList = [...newList];
+}
